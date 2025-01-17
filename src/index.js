d387ac4ea652a835f85a7b9ce327633d5566dcc6
Galih066 <galihdapaimanda@gmail.com>
2025-01-17 22:49:48 +0800

    chore(Package): add dotenve setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+const { PORT } = process.env;
 const app = express();
-const port = 3000;
+const port = PORT || 3000;
 
 app.get('/', (req, res) => {
     res.status(200).json({status: "Success"});
